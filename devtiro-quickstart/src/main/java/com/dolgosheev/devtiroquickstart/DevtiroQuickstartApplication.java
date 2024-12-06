90a5a9b06df5fa9345b11fa1fcdbc63f03398321
Aleksandr Dolgosheev <al.dolgosheev@gmail.com>
2024-12-06 15:55:32 +0300

    2024.12.06 initial commit

---
+++ devtiro-quickstart/src/main/java/com/dolgosheev/devtiroquickstart/DevtiroQuickstartApplication.java
@@ -0,0 +1,13 @@
+package com.dolgosheev.devtiroquickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevtiroQuickstartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevtiroQuickstartApplication.class, args);
+    }
+
+}
---
+++ devtiro-quickstart/src/test/java/com/dolgosheev/devtiroquickstart/DevtiroQuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dolgosheev.devtiroquickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevtiroQuickstartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
